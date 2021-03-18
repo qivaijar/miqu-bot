59ba507d28ddeca6436fe247879bf3e0ca6a93d8
rizki-ginanjar <rizki.ginanjar@prosa.ai>
2021-03-18 21:22:17 +0700

    added the function to respond to user message

--- miqu_bot.py
+++ miqu_bot.py
@@ -10,6 +10,7 @@ load_dotenv()
 # get discord token and specific guild name
 token = os.getenv('miqu_token')
 target_guild = os.getenv('guild_name')
+commands_list = os.getenv('commands_list')
 
 client = discord.Client()
 
@@ -20,6 +21,19 @@ async def on_ready():
         print(f'{client.user} is connected to {guild.name}')
     else:
         sys.exit("Target guild does not match")
+
+@client.event
+async def on_message(message):
+    # To make sure that the bot won't respond to its own messages
+    if message.author == client.user:
+        return
+
+    typed_message = message.content
+    if typed_message.startswith('mq'):
+        if typed_message.split(" ")[0] not in commands_list:
+            await message.channel.send("Miku, Wakannai yo~")
+        else:
+            await message.channel.send("Miku miku dayo~")
     
 
 client.run(token)
