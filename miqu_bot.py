e666edee499d9eacbbe298999176487d07f4fb69
rizki-ginanjar <rizki.ginanjar@prosa.ai>
2021-03-18 20:34:44 +0700

    added scenario to only connect to the targeted guild/server

--- miqu_bot.py
+++ miqu_bot.py
@@ -1,15 +1,27 @@
 # import libraries
-import os
 import discord
 from dotenv import load_dotenv
+import os
+import sys
 
+# load .env file (containing secret info)
 load_dotenv()
+
+# get discord token and specific guild name
 token = os.getenv('miqu_token')
+target_guild = os.getenv('guild_name')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == target_guild:
+            break
+        else:
+            sys.exit("Target guild is not found")
+    
+    print(f'{client.user} is connected to {guild.name}')
+    
 
 client.run(token)
