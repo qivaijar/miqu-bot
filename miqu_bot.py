1bfd653851353f6a14a2fdfd1ecf6f011cd8376b
rizki-ginanjar <rizki.ginanjar@prosa.ai>
2021-03-20 14:04:42 +0700

    updated to miqu-bot v2

--- miqu_bot.py
+++ miqu_bot.py
@@ -1,8 +1,10 @@
 # import libraries
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
-from modules import command_container as cc
+# from modules import command_container as cc
 from modules import command_list as cl
+from modules import music_commands as mc
 import os
 import re
 import sys
@@ -16,9 +18,11 @@ target_guild = os.getenv('guild_name')
 command_list = os.getenv('command_list')
 command_list = command_list.split(" ") # split the string separated by space to get the list of all commands
 trigger_word = os.getenv('trigger_word')
+music_channel = os.getenv('music_channel')
 
-client = discord.Client()
-# 
+client =  commands.Bot(command_prefix=commands.when_mentioned_or("mq"),
+                   description='Semijipun First Discord Bot. Typed \'mq\' + one of the command below to interact:')
+
 @client.event
 async def on_ready():
     guild = discord.utils.find(lambda g: g.name == target_guild, client.guilds)
@@ -27,24 +31,6 @@ async def on_ready():
     else:
         sys.exit("Target guild does not match")
 
-@client.event
-async def on_message(message):
-    # To make sure that the bot won't respond to its own messages
-    if message.author == client.user:
-        return
-
-    typed_message = message.content
-    if typed_message.startswith(trigger_word):
-        typed_command = typed_message.split(" ")[0]
-        input_message = typed_message.split(" ")[1::]
-        if len(input_message) > 1:
-            input_message = " ".join(input_message)
-
-        if len(re.findall(r"(?=("+'|'.join(command_list)+r"))", typed_command)) == 0:
-            await message.channel.send("Miku, Wakannai yo~. type mqhelp to see the full command lists")
-        else:
-            com_con = cc.command_container(typed_command, input_message, client)
-            com_con = com_con.run()
-            await eval(com_con)
-            
+client.add_cog(mc.Music(client))
+client.add_cog(cl.General(client))
 client.run(token)
