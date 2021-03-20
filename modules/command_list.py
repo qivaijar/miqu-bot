59590ca5cde836fe8314209ba1469f40de9db7d3
rizki-ginanjar <rizki.ginanjar@prosa.ai>
2021-03-20 08:56:32 +0700

    updated command_list and text_list

--- modules/command_list.py
+++ modules/command_list.py
@@ -12,9 +12,13 @@ class CommandList:
         self.client = client
 
     def mqhelp(*args):
-        command_string = "message.channel.send(\"" + text_list['mqhelp_strings'][0] + "\")"
+        command_string = "message.channel.send(\"" + text_list['mqhelp_strings'] + "\")"
         return command_string
     
+    def mqhelp(*args):
+        command_string = "message.channel.send(\"" + text_list['mqmu_strings'] + "\")"
+    return command_string
+
     def mqpl(self):
         print(self.client)
         print(self.inp)
